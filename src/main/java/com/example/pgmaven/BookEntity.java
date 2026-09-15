d3fd8beaaf35e9c7a235d35cdfd5142975a3d8a7
USER <walterebelle4@gmail.com>
2026-09-15 22:14:44 +0100

    Fix total de L'Entity, dependences installees, BD cree

--- src/main/java/com/example/pgmaven/BookEntity.java
+++ src/main/java/com/example/pgmaven/BookEntity.java
@@ -1,9 +1,14 @@
 package com.example.pgmaven;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
  
     Long id;
     String name;
